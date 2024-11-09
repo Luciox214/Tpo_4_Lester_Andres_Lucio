c551023de81ed267cf59873bd8480b9ea959f19c
Luciox214 <105755717+Luciox214@users.noreply.github.com>
2024-11-09 12:48:57 -0300

    Singleton DescuentoController
    Modifiqué la clase para que se aplique Singleton.

--- src/main/java/controllers/DescuentoController.java
+++ src/main/java/controllers/DescuentoController.java
@@ -18,15 +18,21 @@ public class DescuentoController {
 	
 	private List<CondicionesDescuento> Descuento;
 	
-    public DescuentoController() {
+    private DescuentoController() {
     	Descuento = new ArrayList<CondicionesDescuento>();
     	CondicionesDescuento CondicionesDescuento = new CondicionesDescuento(new Date(), new Date(), 5, 50, TipoTarjeta.PAMI, new ArrayList<TarjetaDescuento>());
     	Descuento.add(CondicionesDescuento);
     	
     }
+	public static DescuentoController getDescuentoControllerInstance(){
+		if (instancia == null) {
+			instancia = new DescuentoController();
+		}
+		return instancia;
+	}
 
     /**
-     * 
+     * Lucio: Modifiqué el constructor para que sea privado, y cree el método getInstance para acceder a la instancia
      */
     public void ABM() {
 	}
